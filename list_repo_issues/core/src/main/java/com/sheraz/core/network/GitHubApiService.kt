710d464f0c38f5434388551aef8d9d6488ba27ad
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-08-29 11:59:39 +0500

    Create singleton instance of GitHubApiService using Retrofit.

--- list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubApiService.kt
+++ list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubApiService.kt
@@ -1,8 +1,16 @@
 package com.sheraz.core.network
 
+import android.content.Context
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.sheraz.core.network.response.GetGitHubRepoIssuesResponse
 import kotlinx.coroutines.Deferred
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,5 +21,32 @@ interface GitHubApiService {
         @Query("PageSize") PageSize: Int,
         @Query("Page") Page: Int
     ): Deferred<Response<GetGitHubRepoIssuesResponse>>
+
+
+    companion object {
+
+        @Volatile
+        private var instance: GitHubApiService? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context): GitHubApiService = instance ?: synchronized(LOCK) {
+            return instance ?: buildApi(context).also { instance = it }
+        }
+
+        private fun buildApi(context: Context) =
+            Retrofit.Builder()
+                .client(getOkHttpClient(context))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create(Gson()))
+                .build().create(GitHubApiService::class.java)
+
+        private fun getOkHttpClient(context: Context) =
+            OkHttpClient.Builder()
+                .cache(Cache(context.cacheDir, 10*1024*1024))
+                .addInterceptor(HttpLoggingInterceptor(HttpLogger()))
+                .build()
+
+
+    }
 
 }
