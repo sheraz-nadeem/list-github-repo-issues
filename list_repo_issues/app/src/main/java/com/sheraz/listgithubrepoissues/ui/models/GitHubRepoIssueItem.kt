150833541d7188e53b08ca007b2aa993734ecb03
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-08-29 10:55:12 +0500

    Removed interface for UI model. For data class, we don't really need an interface as it holds just the data, no state management logic.

--- list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ui/models/GitHubRepoIssueItem.kt
+++ list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ui/models/GitHubRepoIssueItem.kt
@@ -1,24 +1,25 @@
 package com.sheraz.listgithubrepoissues.ui.models
 
-interface GitHubRepoIssueItem {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-    /**
-     * Items to be shown in the UI
-     * It's not a good practice to pass
-     * around whole database objects
-     * or list of objects, as these objects
-     * certainly have lot of stuff we don't
-     * want in the UI
-     */
-
-    val id: Int
-    val issueId: Int
-    val title: String
-    val body: String
-    val state: String
-    val authorLogin: String
-    val createdAt: String
-    val comments: Int
+@Parcelize
+data class GitHubRepoIssueItem(
+    val id: Int,
+    val issueId: Int,
+    val title: String,
+    val body: String,
+    val state: String,
+    val authorLogin: String,
+    val createdAt: String,
+    val comments: Int,
     val repositoryUrl: String
+) : Parcelable {
 
+    override fun equals(other: Any?): Boolean {
+
+        val item = other as GitHubRepoIssueItem
+        return (item.id == this.id && item.issueId == this.issueId)
+
+    }
 }
