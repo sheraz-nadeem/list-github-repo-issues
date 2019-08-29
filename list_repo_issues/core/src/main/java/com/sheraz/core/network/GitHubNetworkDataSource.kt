8cc7bd5fec0167431b9247cca589b9d340c7f8ee
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-08-29 13:33:12 +0500

    A public method GitHubNetworkDataSource#loadRepoIssuesFromNetwork added which uses fetchRepoIssuesFromNetwork() method to initiate network call and return a response. Also added documentation of these two methods.

--- list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubNetworkDataSource.kt
+++ list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubNetworkDataSource.kt
@@ -4,7 +4,7 @@ import com.sheraz.core.network.response.GetGitHubRepoIssuesResponse
 
 interface GitHubNetworkDataSource {
 
-    suspend fun fetchRepoIssuesFromNetwork(repoName: String, pageSize: Int, page: Int): GetGitHubRepoIssuesResponse
+    suspend fun loadRepoIssuesFromNetwork(repoFullName: String, pageSize: Int, page: Int): GetGitHubRepoIssuesResponse
 
     companion object {
         const val ERROR_MESSAGE = "Error loading github repos data"
--- list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubNetworkDataSourceImpl.kt
+++ list_repo_issues/core/src/main/java/com/sheraz/core/network/GitHubNetworkDataSourceImpl.kt
@@ -1,16 +1,59 @@
 package com.sheraz.core.network
 
 import com.sheraz.core.network.response.GetGitHubRepoIssuesResponse
+import com.sheraz.core.utils.Logger
 
 
-class GitHubNetworkDataSourceImpl: GitHubNetworkDataSource {
+class GitHubNetworkDataSourceImpl(
+    private val logger: Logger,
+    private val gitHubApiService: GitHubApiService
+) : GitHubNetworkDataSource {
 
     /**
-     * This method uses [GitHubApiService] to initiate network fetch
-     * for the provided GitHub repository name
+     * Public method to initiate network request. Since, we are
+     * doing IO operations with API using Retrofit, this method should
+     * be a suspend function and called from a coroutine or another suspend function.
+     *
+     * @param repoFullName String Full Name of the GitHub repository
+     * @param pageSize Int count for number of items to include in response
+     * @param page Int used for page number
+     *
+     * @return [GetGitHubRepoIssuesResponse] response object
      */
-    override suspend fun fetchRepoIssuesFromNetwork(repoName: String, pageSize: Int, page: Int): GetGitHubRepoIssuesResponse {
-        // TODO Implementation
+    override suspend fun loadRepoIssuesFromNetwork(repoFullName: String, pageSize: Int, page: Int): GetGitHubRepoIssuesResponse {
+
+        logger.d(TAG, "loadRepoIssuesFromNetwork(): repoFullName: $repoFullName, pageSize: $pageSize, page: $page")
+        return fetchRepoIssuesFromNetwork(repoFullName, pageSize, page)
+
+    }
+
+    /**
+     * Private method to get Repos using
+     * {@see com.sheraz.core.network.GitHubApiService#getRepoIssues} and return
+     * the received list of GitHub Repository issues
+     *
+     * @param repoFullName String Full Name of the GitHub repository
+     * @param pageSize Int count for number of items to include in response
+     * @param page Int used for page number
+     *
+     * @return [GetGitHubRepoIssuesResponse] response object
+     */
+    private suspend fun fetchRepoIssuesFromNetwork(repoFullName: String, pageSize: Int, page: Int): GetGitHubRepoIssuesResponse {
+
+        logger.d(TAG, "fetchRepoIssuesFromNetwork(): repoFullName: $repoFullName, pageSize: $pageSize, page: $page")
+
+        val response = gitHubApiService.getRepoIssues(repoFullName, pageSize, page).await()
+
+        if (response.isSuccessful) {
+
+            val responseBody = response.body()
+            if (responseBody != null) {
+                logger.i(TAG, "fetchRepoIssuesFromNetwork(): response: $responseBody")
+                return responseBody
+            }
+
+        }
+
         return GetGitHubRepoIssuesResponse(emptyList())
     }
 
