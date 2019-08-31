15372ca83981c0ffb15779d227edb4b7608ea2b1
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-08-31 18:37:26 +0500

    Our Application class now provides CoreComponent & AppComponent instances which can be used for injecting dependencies using an Injector class.

--- list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ListGitHubRepoIssuesApp.kt
+++ list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ListGitHubRepoIssuesApp.kt
@@ -1,20 +1,37 @@
 package com.sheraz.listgithubrepoissues
 
 import android.app.Application
+import com.sheraz.core.di.component.CoreComponent
+import com.sheraz.core.di.component.DaggerCoreComponent
+import com.sheraz.core.di.module.ContextModule
+import com.sheraz.listgithubrepoissues.di.component.AppComponent
+import com.sheraz.listgithubrepoissues.di.component.DaggerAppComponent
 
 class ListGitHubRepoIssuesApp : Application() {
+
+    lateinit var appComponent: AppComponent
+        private set
+    lateinit var coreComponent: CoreComponent
+        private set
 
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+        coreComponent = DaggerCoreComponent.builder()
+            .contextModule(ContextModule(this))
+            .build()
+        appComponent = DaggerAppComponent.builder()
+            .coreComponent(coreComponent)
+            .build()
+
+        coreComponent.logger().d(TAG, "onCreate: ")
     }
 
     companion object {
-
         private val TAG = ListGitHubRepoIssuesApp::class.java.simpleName
+
         private var INSTANCE: ListGitHubRepoIssuesApp? = null
 
         fun get(): ListGitHubRepoIssuesApp = INSTANCE!!
-
     }
 }
