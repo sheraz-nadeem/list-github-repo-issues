7307bd761cf9efc62a06bea254d398a78a48014e
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-09-08 21:05:56 +0500

    Boundary callback now tells AppRepository to reset noMoreItemsAvailable status when needed.

--- list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ui/modules/base/BaseBoundaryCallback.kt
+++ list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ui/modules/base/BaseBoundaryCallback.kt
@@ -15,13 +15,13 @@ abstract class BaseBoundaryCallback<GENERIC_BOUNDARY_CALLBACK_PARCELABLE: Parcel
      * Abstract method that must be implemented by child classes
      * to perform tasks that they need to perform when needed
      */
-    abstract fun requestData(isLastItem: Boolean)
+    abstract fun requestData(isLastItem: Boolean, doResetNoMoreItemsAvailable: Boolean)
 
 
     /**
      * Database returned 0 items. We should query the backend for more items.
      */
-    override fun onZeroItemsLoaded() = requestData(false)
+    override fun onZeroItemsLoaded() = requestData(isLastItem = false, doResetNoMoreItemsAvailable = true)
 
     /**
      * When all items in the database were loaded, we need to query the backend for more items.
@@ -31,7 +31,7 @@ abstract class BaseBoundaryCallback<GENERIC_BOUNDARY_CALLBACK_PARCELABLE: Parcel
         logger.d(TAG, "onItemAtEndLoaded(): noMoreItemsAvailable = ${noMoreItemsAvailable}, itemAtEnd = $itemAtEnd")
         when (noMoreItemsAvailable) {
             true -> return
-            false -> requestData(true)
+            false -> requestData(isLastItem = true, doResetNoMoreItemsAvailable = false)
         }
     }
 
