31871d97a2471d11544d9bd6432fc326ae565744
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2019-09-08 12:25:04 +0500

    Show/Hide keyboard methods added in Utils.

--- list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/utils/Utils.kt
+++ list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/utils/Utils.kt
@@ -1,12 +1,16 @@
 package com.sheraz.listgithubrepoissues.utils
 
 import android.app.Dialog
+import android.app.Service
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.LayerDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.util.DisplayMetrics
 import android.os.Build
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.annotation.RequiresApi
 
 
@@ -31,6 +35,30 @@ fun setWhiteNavigationBar(dialog: Dialog) {
         windowBackground.setLayerInsetTop(1, metrics.heightPixels)
 
         window.setBackgroundDrawable(windowBackground)
+    }
+
+}
+
+fun hideKeyboard(context: Context, et: EditText) {
+    try {
+        et.clearFocus()
+        val imm = context.getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(et.windowToken, 0)
+    } catch (e: Exception) {
+    }
+
+}
+
+fun showKeyboard(context: Context, et: EditText) {
+    try {
+        et.requestFocus()
+        val imm = context.getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(et, 0)
+        imm.toggleSoftInput(
+            InputMethodManager.SHOW_FORCED,
+            InputMethodManager.HIDE_IMPLICIT_ONLY
+        )
+    } catch (e: Exception) {
     }
 
 }
