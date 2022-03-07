e84f6377dc656e2ef9d8c80e14c4cefc1c43e06d
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2022-03-07 18:26:08 +0100

    clean up

--- list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ListGitHubRepoIssuesApp.kt
+++ list_repo_issues/app/src/main/java/com/sheraz/listgithubrepoissues/ListGitHubRepoIssuesApp.kt
@@ -6,29 +6,8 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class ListGitHubRepoIssuesApp : Application() {
 
-//    lateinit var appComponent: AppComponent
-//        private set
-//    lateinit var coreComponent: CoreComponent
-//        private set
-
     override fun onCreate() {
         super.onCreate()
-//        INSTANCE = this
-//        coreComponent = DaggerCoreComponent.builder()
-//            .contextModule(ContextModule(this))
-//            .build()
-//        appComponent = DaggerAppComponent.builder()
-//            .coreComponent(coreComponent)
-//            .build()
-//
-//        coreComponent.logger().d(TAG, "onCreate: ")
     }
 
-//    companion object {
-//        private val TAG = ListGitHubRepoIssuesApp::class.java.simpleName
-//
-//        private var INSTANCE: ListGitHubRepoIssuesApp? = null
-//
-//        fun get(): ListGitHubRepoIssuesApp = INSTANCE!!
-//    }
 }
