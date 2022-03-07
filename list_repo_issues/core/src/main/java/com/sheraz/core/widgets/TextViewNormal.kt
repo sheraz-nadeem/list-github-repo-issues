b859a458d7b21cc2b00c420b74ca1547ba04d760
sheraz-nadeem <sheraz_nadeem04@yahoo.com>
2022-03-07 20:41:50 +0100

    Example of @WithFragmentBindings usage to make available fragment scoped bindings.

--- list_repo_issues/core/src/main/java/com/sheraz/core/widgets/TextViewNormal.kt
+++ list_repo_issues/core/src/main/java/com/sheraz/core/widgets/TextViewNormal.kt
@@ -4,34 +4,39 @@ import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import com.sheraz.core.utils.Logger
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.WithFragmentBindings
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class TextViewNormal : AppCompatTextView {
+@AndroidEntryPoint
+@WithFragmentBindings
+class TextViewNormal @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    @ApplicationContext
+    @Inject
+    lateinit var appContext: Context
+
+    @Inject
+    lateinit var logger: Logger
 
     private var fontNormal: Typeface? = null
 
-    constructor(context: Context) : super(context) {
-        fontNormal = Typeface.createFromAsset(context.assets, "fonts/Geogtq-Lg.otf")
-        setCustomFont()
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        fontNormal = Typeface.createFromAsset(context.assets, "fonts/Geogtq-Lg.otf")
-        setCustomFont()
-    }
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    init {
         fontNormal = Typeface.createFromAsset(context.assets, "fonts/Geogtq-Lg.otf")
         setCustomFont()
     }
 
     private fun setCustomFont() {
-
+        logger.d(TAG, "setCustomFont: context = $context, appContext = $appContext")
         val typeface = typeface
         setTypeface(fontNormal)
 
         if (typeface.isItalic)
             setTypeface(getTypeface(), Typeface.ITALIC)
     }
-
-
 }
+private const val TAG = "TextViewNormal"
